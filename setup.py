d59a48ab1aa51a4e443262cd703beb47e5ebe5a2
MrP01 <peter@waldert.at>
2015-11-14 18:28:26 +0100

    Adding setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name="sqassistant",
+	packages=["sqassistant"],
+	version="0.1",
+	description="Start and quit your applications",
+	author="Peter Waldert",
+	author_email="peter@waldert.at",
+	url="https://github.com/MrP01/StartQuitAssistant",
+	download_url="https://github.com/MrP01/StartQuitAssistant/tarball/v0.1",
+	keywords=["application", "session"],
+	classifiers=[]
+)
